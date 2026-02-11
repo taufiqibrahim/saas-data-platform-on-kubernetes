46b6c0ce914fcb9a682de94dab94e74a422462d9
Taufiq Ibrahim <taufiq.ibrahim@gmail.com>
2026-02-11 20:29:42 +0700

    control-plane: working docker build

--- control-plane-api/entrypoint.sh
+++ control-plane-api/entrypoint.sh
@@ -1,24 +1,18 @@
 #!/bin/sh
 set -e
 
-echo $PWD
+# If CMD arguments are passed (e.g. "temporal" or "api"), prefer that
+if [ -n "$1" ]; then
+  TARGET="$1"
+fi
 
-echo ls -lh
-
-sleep infinity
-
-# # If CMD arguments are passed (e.g. "temporal" or "api"), prefer that
-# if [ -n "$1" ]; then
-#   TARGET="$1"
-# fi
-
-# case "$TARGET" in
-#   temporal)
-#     echo "🌀 Starting Temporal Worker..."
-#     exec node dist/temporal/workers/worker.worker.js
-#     ;;
-#   api|*)
-#     echo "🚀 Starting API Server..."
-#     exec node dist/index.js
-#     ;;
-# esac
+case "$TARGET" in
+  temporal)
+    echo "🌀 Starting Temporal Worker..."
+    exec node dist/src/temporal/workers/worker.worker.js
+    ;;
+  api|*)
+    echo "🚀 Starting API Server..."
+    exec node dist/src/index.js
+    ;;
+esac
